994246b95d9916efeec3316c1a8b8be75958c572
Tafari Byfield <tbyfield001@gmail.com>
2025-05-17 01:55:25 -0400

    Added major return function, cleaned up some redundent syntax, implemented robots 1&2 into movement function, as it was created before the robot identity.

--- robots/movement.py
+++ robots/movement.py
@@ -1,7 +1,6 @@
 from farm import layout
 import random
-
-from farm.layout import display_grid
+from robots.robots import robot1, robot2, holding_fruit
 
 
 #define directions, up down left, right
@@ -21,18 +20,66 @@ def find_valid_moves(grid,r,c):
     return moves
 #*************************************************************************************************
 
+
+
+#return to orignal position
+def return_to_start(robot, grid):
+    if robot['holding_fruit'] == False:
+        return
+    x,y = robot['position']
+    sx, sy = robot['starting_point']
+
+    # deliver message when back at original position
+    if (x, y) == (sx, sy):
+        print(f"Fruit delivered by Robot {robot['id']}")
+        robot['holding_fruit'] = False
+        return
+
+    dx = sx - x
+    dy = sy - y
+
+    #moving horizontolly first
+    if dx != 0:
+        step_x = x + (1 if (dx > 0) else - 1)
+        if grid[step_x][y] is None:
+            grid[x][y] = None
+            robot['position'] = (step_x, y)
+            grid[step_x][y] = f"R{robot['id']}"
+            return
+    #vertical movements
+    if dy != 0:
+        step_y = y + (1 if (dy > 0) else - 1)
+        if grid[x][step_y] is None:
+            grid[x][y] = None
+            robot['position'] = (x, step_y)
+            grid[x][step_y] = f"R{robot['id']}"
+            return
+
+#************************************************************************
+
 #movement
 def movement(grid,robot_positions):
-    for i,(r,c) in enumerate(robot_positions):  #fill spots if empty
-        valid_moves = find_valid_moves(grid,r,c)
+    robots = [robot1,robot2]
 
+    for i,(r,c) in enumerate(robot_positions):  #fill spots if empty
+        robot = robots[i]
+
+        if robot['holding_fruit']:
+            return_to_start(robot, grid)
+            robot_positions[i] = robot['position']
+            continue
+
+        valid_moves = find_valid_moves(grid,r,c)
         if valid_moves:
             new_row,new_col = random.choice(valid_moves)    #random movement
 
             grid[r][c] = None #make old pos avaliable
+
             #update pos of robots
             robot_positions[i] = (new_row,new_col)
             grid[new_row][new_col] = f"R{i+1}"
+
+            robot['position'] = (new_row, new_col) #reset
 
     return grid,robot_positions
 #*************************************************************************************************
